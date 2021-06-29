048dcc4fbaac59cf4b07dca5921d9d0b7d8a4916
Carlos Daniel <dev.cdfaria@gmail.com>
2021-06-29 14:49:02 -0300

    modal functions

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
 
+const modalOverlayEl = document.getElementById('modal-overlay')
+const modalMessageEl = document.getElementById('message')
+const modalButton = document.getElementById('close-modal')
+
 const randomFunc = {
   upper: getRandomUpper,
   lower: getRandomLower,
@@ -14,40 +18,15 @@ const randomFunc = {
   symbol: getRandomSymbol
 }
 
-clipboardEl.addEventListener('click', () => {
-  const textarea = document.createElement('textarea')
-  const password = resultEl.innerText
-
-  if (!password) return
-
-  textarea.value = password
-
-  document.body.appendChild(textarea)
-  textarea.select()
-  document.execCommand('copy')
-  textarea.remove()
-  alert('Password copied to clipboard!')
-})
-
-generateEl.addEventListener('click', () => {
-  const length = Number(lengthEl.value)
-  const hasUpper = uppercaseEl.checked
-  const hasLower = lowercaseEl.checked
-  const hasNumber = numbersEl.checked
-  const hasSymbol = symbolsEl.checked
-
-  resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length)
-})
-
-
 function generatePassword(upper, lower, number, symbol, length) {
-  let generatedPassword = []
+  let generatedPasswordArray = []
   let finalPassword = ''
 
   const typesCount = upper + lower + number + symbol
   const typesArray = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0])
 
   if (typesCount === 0 ) {
+    showModal('Can\'t generate password without types')
     return ''
   }
 
@@ -56,15 +35,14 @@ function generatePassword(upper, lower, number, symbol, length) {
       const funcName = Object.keys(type)[0]
       const key = randomFunc[funcName]()
 
-      Math.round(Math.random() * 1) ? generatedPassword.unshift(key) : generatedPassword.push(key)
+      Math.round(Math.random()) ? generatedPasswordArray.unshift(key) : generatedPasswordArray.push(key)
     })
   }
 
-  generatedPassword.forEach(key => finalPassword += key)
+  generatedPasswordArray.forEach(key => finalPassword += key)
 
   return finalPassword
 }
-
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -82,3 +60,38 @@ function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.'
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
+
+function showModal(message) {
+  modalMessageEl.innerText = message
+  modalOverlayEl.classList.add("show-modal")
+}
+
+clipboardEl.addEventListener('click', () => {
+  const textarea = document.createElement('textarea')
+  const password = resultEl.innerText
+
+  if (!password) {
+    showModal("Generate a password before copy")
+    return
+  }
+
+  textarea.value = password
+
+  document.body.appendChild(textarea)
+  textarea.select()
+  document.execCommand('copy')
+  textarea.remove()
+  showModal('Password copied to clipboard!')
+})
+
+generateEl.addEventListener('click', () => {
+  const length = Number(lengthEl.value)
+  const hasUpper = uppercaseEl.checked
+  const hasLower = lowercaseEl.checked
+  const hasNumber = numbersEl.checked
+  const hasSymbol = symbolsEl.checked
+
+  resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length)
+})
+
+modalButton.addEventListener('click', () => modalOverlayEl.classList.contains('show-modal') ? modalOverlayEl.classList.remove('show-modal') : '')
